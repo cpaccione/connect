3a684c29f0bd39a6bf8cb685061df478a3784d75
Frost <cpaccione77@gmail.com>
2016-05-16 17:55:48 -0500

    contact page updates

--- connectite/page-contact.php
+++ connectite/page-contact.php
@@ -35,5 +35,26 @@ Template Name: Contact
     </div>
 </section>
 
+<section class="contact-form">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-5 col-md-push-7">
+				<div class="contact-form-sidebar">
+					<p>Charlotte Pipe<br>
+					and Foundry Company<br>
+					2109 Randolph Road<br>
+					Charlotte, NC 28207</p>
+					<p>000-000-000<br>
+					<a href="mailto:info@connectite.com">info@connectite.com</a></p>
+					<p><a href="/store-locations/">Store Locations</a></p>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-7 col-md-pull-5">
+				<?php echo do_shortcode( '[contact-form-7 id="1234" title="Connectite Form"]' ); ?>
+			</div>
+		</div>
+	</div>
+</section>
+
 
 <?php get_footer(); ?>
